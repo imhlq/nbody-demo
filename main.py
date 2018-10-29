447043080112ca96cb6a0abe85f8e2e6f71c3b04
Richard Hou <im_hlq@163.com>
2018-10-29 11:48:11 -0400

    add note

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import numpy as np
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
-box_shape = {'type': 'normal', 'box_size': (20, 20)}
-#box_shape = {'type': 'lattice', 'lattice_size': (4, 4), 'lattice_length': 2.5}
-particle_num = 100
-max_vi = 1.5    # init velocity
-delta_t = 0.005
-mybox = Experiment2DBox(box_shape, particle_num, max_vi, potential='Gravity')
-tstep = 8000
+#### Change Parameter Here ####
+box_shape = {'type': 'normal', 'box_size': (20, 20)}    # shape of Box (W * H)
+particle_num = 20  # Initial Total Number of particle
+max_vi = 1.5    # Init velocity
+delta_t = 0.005 # Time interval
+tstep = 8000    # Flame
 
+#### Dont change below if you don't know #### 
+
+mybox = Experiment2DBox(box_shape, particle_num, max_vi, potential='Gravity')
 ## Animation Create
 fig = plt.figure()
 ax = plt.axes(xlim=(0, mybox.box_size[0]), ylim=(0, mybox.box_size[1]))
